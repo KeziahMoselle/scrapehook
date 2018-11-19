9f74989f1f4fa9213236c2be8fd2c934e512bc2b
KeziahMoselle <keziah.moselle@gmail.com>
2018-11-19 20:24:00 +0100

    Update for npm

--- index.js
+++ index.js
@@ -1,14 +1,86 @@
-const Webhook = require('./src/webhook')
+const EventEmitter = require('events')
+const https = require('https')
 
-Webhook.observe({
-  url: 'https://keziahmoselle.fr/',
-  interval: 10*1000
-})
+/**
+ * Webhook class
+ *
+ * @class Webhook
+ * @extends {EventEmitter}
+ */
+class Webhook extends EventEmitter {
+  /**
+   * Initialize the Webhook
+   * Set URL
+   * Set interval
+   * Set firstRequest to true
+   * Set oldBody
+   * Begin to scrape
+   *
+   * @param {object} data
+   * @memberof Webhook
+   */
+  observe (data) {
+    this.url = data.url
+    this.interval = data.interval || 5*60*1000
+    this.firstRequest = true
+    this.oldBody = ''
+    this.scrape()
+  }
 
-Webhook.on('diff', () => {
-  console.log('diff')
-})
+  /**
+   * Fetch the body of a page
+   * Executed every this.interval (default: 5min)
+   * Call compare() when response end
+   *
+   * @memberof Webhook
+   */
+  scrape () {
+    setInterval(() => {
 
-Webhook.on('nodiff', () => {
-  console.log('nodiff')
-})
+      https.get(this.url, (response) => {
+        let body = ''
+
+        response.on('data', (chunk) => {
+          body += chunk
+        })
+
+        response.on('end', () => {
+          if (this.firstRequest) {
+            console.log('Set oldBody for the first time')
+            this.oldBody = body
+            return this.firstRequest = false
+          }
+          this.compare(body, this.oldBody)
+        })
+      }).on('error', (error) => {
+        throw new Error(error)
+      })
+
+    }, this.interval);
+  }
+  
+  /**
+   * Return true when the oldBody is equal to the actual body received
+   * Emit 'nodiff' when body === oldBody
+   * Emit 'diff' when body !== oldBody
+   *
+   * @param {string} body
+   * @param {string} oldBody
+   * @returns {boolean}
+   * @memberof Webhook
+   */
+  compare (body, oldBody) {
+    if (body === oldBody) {
+      this.oldBody = body
+      this.emit('nodiff')
+      return true
+    } else {
+      this.oldBody = body
+      this.emit('diff')
+      return false
+    }
+  }
+  
+}
+
+module.exports = new Webhook()
